7ef8e2cbc0567fcb0c6832510877db1c41155e88
Manfred Eppe <eppe@informatik.uni-hamburg.de>
2017-06-09 12:20:25 +0200

    remnoved unnecessary gitignore file

--- run_training.sh
+++ run_training.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker rm ros_frcnn_training
 echo "Note: Training must be run with nvidia-docker and GPU-support because some layers in caffe do not support cpu mode. Running nvidia-docker with GPU"
+docker pull meppe78/ros-kinetic-frcnn-training
 nvidia-docker run \
 --rm \
 -v /$(pwd)/src/frcnn:/opt/ros-ort/src/frcnn \
