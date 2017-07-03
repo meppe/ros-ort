69cde33dae6f3b87099fbf88a36c89433ecc2081
Manfred Eppe <eppe@informatik.uni-hamburg.de>
2017-07-03 15:07:41 +0200

    repaired video stream entrypoint

--- frcnn_entrypoint.sh
+++ frcnn_entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+# setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/opt/ros-ort/devel/setup.bash"
 export PYTHONPATH="$PYTHONPATH:/opt/ros/$ROS_DISTRO/lib/python2.7/dist-packages"
 export ROS_MASTER_URI=http://roscore_kinetic:11311
 export ROS_IP=`hostname -I`
-exec $@
 
+$@
