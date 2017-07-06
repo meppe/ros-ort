f369de82e2ea155b82380fb90f40c17576a07a64
Manfred Eppe <eppe@informatik.uni-hamburg.de>
2017-07-06 16:29:30 +0200

    fixed download scripts

--- caffemodels/download_nico_faster_rcnn_models.sh
+++ caffemodels/download_nico_faster_rcnn_models.sh
@@ -3,8 +3,10 @@
 #DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
 #cd $DIR
 
-FILE=ZF_faster_rcnn_final.caffemodel
-URL=https://drive.google.com/open?id=0B_Cy6UXhY3PIRDFmcGdMZG1nV0U
+mkdir nico_frcnn_models
+
+FILE="nico_frcnn_models/ZF_faster_rcnn_final.caffemodel"
+URL="https://drive.google.com/uc?export=download&id=0B_Cy6UXhY3PIRDFmcGdMZG1nV0U"
 CHECKSUM=a79fe9f75fd4b25d9b887c3f58a44c93
 #
 if [ -f $FILE ]; then
@@ -34,8 +36,8 @@ wget $URL -O $FILE
 echo "Done. Please run this command again to verify that checksum = $CHECKSUM."
 
 
-FILE=VGG16_faster_rcnn_final.caffemodel
-URL=https://drive.google.com/open?id=0B_Cy6UXhY3PIVkRSRVhHLVlTUWM
+FILE="VGG16_faster_rcnn_final.caffemodel"
+URL="https://drive.google.com/uc?export=download&id=0B_Cy6UXhY3PIVkRSRVhHLVlTUWM"
 CHECKSUM=a79fe9f75fd4b25d9b887c3f58a44c93
 #
 if [ -f $FILE ]; then
@@ -56,7 +58,7 @@ fi
 
 echo "Downloading Faster R-CNN ZF model for NICO (226M)..."
 #
-wget $URL -O $FILE
+wget $URL -O nico_frcnn_models/$FILE
 #
 #echo "Unzipping..."
 #
